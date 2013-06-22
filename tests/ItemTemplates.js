81eacc0ac5f13e9b3ed4a682da7b944f22b1362a
Patrick Smith <mpatricks@gmail.com>
2013-06-22 15:32:58 -0400

    Added classes to buttons

--- tests/ItemTemplates.js
+++ tests/ItemTemplates.js
@@ -7,6 +7,7 @@ suite('ItemTemplates', function() {
 				name: "Laika Test",
 				properties: ["Property1", "Property2", "Property3"]
 			});
+			
 			var docs = itemTemplateCol.find().fetch();
 			emit('docs', docs);
 		});
@@ -33,4 +34,29 @@ suite('ItemTemplates', function() {
 			done();
 		});
 	});
+	
+	test('in the client', function(done, server, client) {
+		client.eval(function() {
+			Meteor.call("createItemTemplate", {
+				name: "Laika Test", 
+				properties: ["Property1", "Property2", "Property3"]
+			},
+			function(error, id) {
+				if(error)
+					emit('error', error);
+				else
+					emit('success', error);
+			});
+		});
+		
+		client.once('error', function(error) {
+			assert.fail(1, 2);
+			done();
+		})
+		
+		client.once('success', function() {
+			assert.ok('ok', 'ok');
+			done();
+		})
+	})
 });
